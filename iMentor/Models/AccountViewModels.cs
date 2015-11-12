62a624215fc03559ba38dc29362cc54068e7f674
drthomas <thomas.1728@yahoo.com>
2015-11-12 12:11:01 -0500

    Restored code that was removed.  Hopefully this fixes the log in issue.

--- iMentor/Models/AccountViewModels.cs
+++ iMentor/Models/AccountViewModels.cs
@@ -23,28 +23,28 @@ namespace iMentor.Models
         public bool RememberMe { get; set; }
     }
 
-    //public class VerifyCodeViewModel
-    //{
-    //    [Required]
-    //    public string Provider { get; set; }
+    public class VerifyCodeViewModel
+    {
+        [Required]
+        public string Provider { get; set; }
 
-    //    [Required]
-    //    [Display(Name = "Code")]
-    //    public string Code { get; set; }
-    //    public string ReturnUrl { get; set; }
+        [Required]
+        [Display(Name = "Code")]
+        public string Code { get; set; }
+        public string ReturnUrl { get; set; }
 
-    //    [Display(Name = "Remember this browser?")]
-    //    public bool RememberBrowser { get; set; }
+        [Display(Name = "Remember this browser?")]
+        public bool RememberBrowser { get; set; }
 
-    //    public bool RememberMe { get; set; }
-    //}
+        public bool RememberMe { get; set; }
+    }
 
-    //public class ForgotViewModel
-    //{
-    //    [Required]
-    //    [Display(Name = "Email")]
-    //    public string Email { get; set; }
-    //}
+    public class ForgotViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
 
     public class LoginViewModel
     {
@@ -62,51 +62,51 @@ namespace iMentor.Models
         public bool RememberMe { get; set; }
     }
 
-    //public class RegisterViewModel
-    //{
-    //    [Required]
-    //    [EmailAddress]
-    //    [Display(Name = "Email")]
-    //    public string Email { get; set; }
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
 
-    //    [Required]
-    //    [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-    //    [DataType(DataType.Password)]
-    //    [Display(Name = "Password")]
-    //    public string Password { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
 
-    //    [DataType(DataType.Password)]
-    //    [Display(Name = "Confirm password")]
-    //    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-    //    public string ConfirmPassword { get; set; }
-    //}
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
 
-    //public class ResetPasswordViewModel
-    //{
-    //    [Required]
-    //    [EmailAddress]
-    //    [Display(Name = "Email")]
-    //    public string Email { get; set; }
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
 
-    //    [Required]
-    //    [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-    //    [DataType(DataType.Password)]
-    //    [Display(Name = "Password")]
-    //    public string Password { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
 
-    //    [DataType(DataType.Password)]
-    //    [Display(Name = "Confirm password")]
-    //    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-    //    public string ConfirmPassword { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
 
-    //    public string Code { get; set; }
-    //}
+        public string Code { get; set; }
+    }
 
-    //public class ForgotPasswordViewModel
-    //{
-    //    [Required]
-    //    [EmailAddress]
-    //    [Display(Name = "Email")]
-    //    public string Email { get; set; }
-    //}
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
 }

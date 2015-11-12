fed24c1c151e67c79829408d64b9d229f574bc19
drthomas <thomas.1728@yahoo.com>
2015-11-12 15:32:07 -0500

    More AngularJS setup

--- iMentor/App_Start/BundleConfig.cs
+++ iMentor/App_Start/BundleConfig.cs
@@ -8,6 +8,9 @@ namespace iMentor
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 

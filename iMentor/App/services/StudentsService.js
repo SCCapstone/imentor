9dc12ceb8aee3f34891158d09b3cf89fb3759933
MICHAEL RICKABAUGH <mikerofsc@gmail.com>
2015-12-01 18:39:28 -0500

    Added the App directory and populated appropriately.

---
+++ iMentor/App/services/StudentsService.js
@@ -0,0 +1,10 @@
+﻿StudentsApp.factory('StudentsService', ['$http', function ($http) {
+    var StudentsService = {};
+
+    StudentsService.getStudent = function () {
+        return $http.get('/Students/GetStudents');
+    };
+
+    return StudentsService;
+
+}]);

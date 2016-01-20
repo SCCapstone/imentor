c9e61aa1d04713d4ed139bcd7005a88740da7d3f
David Thomas <thomas.1728@yahoo.com>
2016-01-20 15:54:21 -0500

    Added some basic filtering

--- iMentor/App/controllers/Home/HomeCtrl.js
+++ iMentor/App/controllers/Home/HomeCtrl.js
@@ -3,6 +3,31 @@
 app.controller('homeCtrl',
     function ($scope, $rootScope, $location, $log, homeService)
     {
-        $scope.message = "Home Page";
+        $scope.subjects = [
+            { 'name': 'Math' },
+            { 'name': 'Science' },
+            { 'name': 'History' },
+            { 'name': 'Reading' }];
+
+        $scope.subjectsIncludes = [];
+
+
+        $scope.includeSubject = function (name) {
+            var i = $.inArray(name, $scope.subjectsIncludes);
+            if (i > -1) {
+                $scope.subjectsIncludes.splice(i, 1);
+            } else {
+                $scope.subjectsIncludes.push(name);
+            }
+        }
+
+        $scope.subjectFilter = function (subjects) {
+            if ($scope.subjectsIncludes.length > 0) {
+                if ($.inArray(subjects.name, $scope.subjectsIncludes) < 0)
+                    return;
+            }
+
+            return subjects;
+        }
     }
 );

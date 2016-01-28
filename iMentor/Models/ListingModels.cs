09ff9a889da01096bd5ae714fe51f5286b10e27c
Ryan Labrador <mrrlabrador@live.com>
2016-01-28 13:51:55 -0500

    Updated listing model.

--- iMentor/Models/ListingModels.cs
+++ iMentor/Models/ListingModels.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace iMentor.Models
 {
     public class ListingModels
     {
-        public int ID { get; set; }
+        public int ListingID { get; set; }
         public string Title { get; set; }
         public string StartDate { get; set; }
         public string EndDate { get; set; }
@@ -15,7 +17,10 @@ namespace iMentor.Models
         public string Frequency { get; set; }
         public string Description { get; set; }
         public string Mentor { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [DataType(DataType.Url)]
         public string URL { get; set; }
-        public string Open { get; set; }
+        public bool Open { get; set; }
     }
 }

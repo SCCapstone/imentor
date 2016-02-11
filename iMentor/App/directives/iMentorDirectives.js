5a9158b17183c2dfc35b520586602a28bbefc835
David Thomas <thomas.1728@yahoo.com>
2016-02-11 12:16:50 -0500

    More cleanup.

--- iMentor/App/directives/iMentorDirectives.js
+++ iMentor/App/directives/iMentorDirectives.js
@@ -23,13 +23,13 @@
 
                 function getImage()
                 {
-                    if (scope.listing.subject == 'Math')
+                    if (scope.listing.area == 'Math')
                         return 'img/Math.png';
-                    else if (scope.listing.subject == 'Science')
+                    else if (scope.listing.area == 'Science')
                         return 'img/Science.png';
-                    else if (scope.listing.subject == 'History')
+                    else if (scope.listing.area == 'History')
                         return 'img/World.png';
-                    else if (scope.listing.subject == 'Reading')
+                    else if (scope.listing.area == 'Reading')
                         return 'img/Reading.png';
                 }
             }

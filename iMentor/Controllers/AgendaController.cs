93e5803aafd6a5c4b8f255292fab35840e67b1d3
MICHAEL RICKABAUGH <mikerofsc@gmail.com>
2016-02-12 00:18:56 -0500

    Agenda Page Model (in two forms), Context, and Controller. Working on getting the rendering to work.

---
+++ iMentor/Controllers/AgendaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using DHTMLX.Scheduler;
+
+namespace iMentor.Controllers
+{
+    public class AgendaController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            var scheduler = new DHXScheduler(this);
+            scheduler.Height = 600;
+            scheduler.Skin = DHXScheduler.Skins.Flat;
+
+            return View(scheduler);
+        }
+    }
+}

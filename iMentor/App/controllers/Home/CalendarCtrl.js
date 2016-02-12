b91442d0c5ec84cbbd4629ab47998caf5eda5d2c
MICHAEL RICKABAUGH <mikerofsc@gmail.com>
2016-02-12 18:00:19 -0500

    Started Calendar population functionality

--- iMentor/App/controllers/Home/CalendarCtrl.js
+++ iMentor/App/controllers/Home/CalendarCtrl.js
@@ -6,9 +6,25 @@ app.controller('calendarCtrl', ['$scope', 'calendarService',
         $scope.userListings = [];
         getListingsByCurrentUser();
 
+        var events = [];
+        /*
         var events = [
                 {title: "Test", start: new Date (2016, 02, 11)}
         ];
+        
+        var length = userListings.length;
+        var tempTitle = "";
+        var tempDate = "";
+        
+        for (var i = 0; i < length; ++i) {
+            userListings[i].Title = tempTitle;
+            userListings[i].StartDate = tempDate;
+
+            events.push({title: tempTitle, start: tempDate})
+        }
+        */
+        events.push({ title: "Test", start: new Date(2016, 02, 11) });
+        //events.push({ title: $scope.userListings[0].Title, start: $scope.userListings[0].StartDate });
 
         $scope.eventSources = [events];
         $scope.calOptions = {

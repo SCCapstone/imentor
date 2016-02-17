fdbf75c481e9383548977fbf92baf9a8a482b443
neighbja <neighbja@email.sc.edu>
2016-02-17 13:29:51 -0500

    why

--- iMentor/App/controllers/Home/CalendarCtrl.js
+++ iMentor/App/controllers/Home/CalendarCtrl.js
@@ -3,42 +3,21 @@
 app.controller('calendarCtrl', ['$scope', 'calendarService', 
     function CalendarCtrl($scope, calendarService, uiCalendarConfig){
 
-      var events = [];
+    $scope.userListings= [];
+    $scope.events = [];
+    getListingsByCurrentUser();
+    console.log($scope.userListings);
+    console.log($scope.userListings.length);
 
-      getListingsByCurrentUser();
-      
-      for (var i = 0; i < userListings.length; i++)
+      for (var i = 0; i < $scope.userListings.length; i++)
       {
           events.push({title: userListings[i].Title, start: userListings[i].StartDate})
       }
  
 
-      //var eventListLength = listings.length;
-        //$scope.numberOfListings = data.length();
-        //$scope.length2 = getListingsByCurrentUser().length;
-
-        //$scope.events = [];
-        /*
-        var events = [
-                {title: "Test", start: new Date (2016, 02, 11)}
-        ];
-        
-        var length = userListings.length;
-        var tempTitle = "";
-        var tempDate = "";
-        
-        for (var i = 0; i < length; ++i) {
-            userListings[i].Title = tempTitle;
-            userListings[i].StartDate = tempDate;
-
-            events.push({title: tempTitle, start: tempDate})
-        }
-        */
-       // events.push({ title: "Test", start: new Date(2016, 02, 11) });
-       //userListings.push({ title: "new Testing", start: new Date(2016,2,16) });
-
-       $scope.eventSources = [userListings];
-       var len = $scope.eventSources.length;
+      
+      $scope.eventSources = [$scope.userListings];
+ 
         $scope.uiConfig = {
             calendar: {
                 height: 900,

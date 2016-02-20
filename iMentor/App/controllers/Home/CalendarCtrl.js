1e9e6af75b34d0f01e1e79b3017ba5d931166955
neighbja <neighbja@email.sc.edu>
2016-02-20 14:15:37 -0500

    hardcoded events

--- iMentor/App/controllers/Home/CalendarCtrl.js
+++ iMentor/App/controllers/Home/CalendarCtrl.js
@@ -3,8 +3,19 @@
 app.controller('calendarCtrl', ['$scope', 'calendarService', 
     function CalendarCtrl($scope, calendarService, uiCalendarConfig){
 
+        var date = new Date();
+        var d = date.getDate();
+        var m = date.getMonth();
+        var y = date.getFullYear();
 
-      
+        $scope.events= [
+           {title: 'All Day Event',start: new Date(y, m, 1)},
+      {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
+      {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
+      {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
+      {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
+      {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+        ];
 
    
         $scope.uiConfig = {
@@ -21,7 +32,7 @@ app.controller('calendarCtrl', ['$scope', 'calendarService',
             }
         };
 
-        $scope.eventSources = [];
+        $scope.eventSources = [$scope.events];
 
         $scope.getListings = function() {
             calendarService.getListingsByCurrentUser()

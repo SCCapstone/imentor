9311929a4135aebf0b98442d7fc0f0f30f0e7c81
David Thomas <thomas.1728@yahoo.com>
2016-03-28 16:59:18 -0400

    More "Add Listing" work

--- iMentor/App/directives/iMentorDirectives.js
+++ iMentor/App/directives/iMentorDirectives.js
@@ -50,26 +50,39 @@
                 templateUrl: '/templates/DateTime.html',
 
                 link: function (scope, elem, attrs) {
+                    
                     scope.daysOfTheWeek = [
                         { value: 1, text: "Monday", abb: "Mon", letter: "M", selected: false },
-                        { value: 1, text: "Tuesday", abb: "Tue", letter: "T", selected: false },
-                        { value: 1, text: "Wednesday", abb: "Wed", letter: "W", selected: false },
-                        { value: 1, text: "Thursday", abb: "Thu", letter: "R", selected: false },
-                        { value: 1, text: "Friday", abb: "Fri", letter: "F", selected: false },
-                        { value: 1, text: "Saturday", abb: "Sat", letter: "S", selected: false },
-                        { value: 1, text: "Sunday", abb: "Sun", letter: "U", selected: false }
+                        { value: 2, text: "Tuesday", abb: "Tue", letter: "T", selected: false },
+                        { value: 3, text: "Wednesday", abb: "Wed", letter: "W", selected: false },
+                        { value: 4, text: "Thursday", abb: "Thu", letter: "R", selected: false },
+                        { value: 5, text: "Friday", abb: "Fri", letter: "F", selected: false },
+                        { value: 6, text: "Saturday", abb: "Sat", letter: "S", selected: false },
+                        { value: 7, text: "Sunday", abb: "Sun", letter: "U", selected: false }
                     ]
 
                     parseFrequency();
 
-                    console.log("Date Time Listing: " + scope.listing.StartDate);
+                    scope.isNew = (scope.listing.Id == null);
 
-                    scope.timeEditMode = false;
-                    scope.startTime = scope.listing.StartDate; 
-                    scope.endTime = scope.listing.EndDate;
-                    scope.startDate = scope.listing.StartDate;
-                    scope.endDate = scope.listing.EndDate;
+                    scope.timeEditMode = (scope.listing.Id == null);
 
+                    scope.startTime = null; 
+                    scope.endTime = null;
+                    scope.startDate = null;
+                    scope.endDate = null;
+
+                    if(!scope.isNew){
+                        scope.startTime = scope.listing.StartDate; 
+                        scope.endTime = scope.listing.EndDate;
+                        scope.startDate = scope.listing.StartDate;
+                        scope.endDate = scope.listing.EndDate;
+                    }else{
+                        scope.startTime = new Date(); 
+                        scope.endTime = new Date();
+                        scope.startDate = new Date();
+                        scope.endDate = new Date();
+                    }
                     
                     scope.picker = { opened: false };
 
@@ -80,6 +93,21 @@
                     };
 
                     scope.closePicker = function () {
+
+                        var startDate = new Date(scope.startDate);
+                        var startTime = new Date(scope.startTime);
+                        var endDate = new Date(scope.endDate);
+                        var endTime = new Date(scope.endTime);
+
+                        startDate.setHours(startTime.getHours());
+                        startDate.setMinutes(startTime.getMinutes());
+
+                        endDate.setHours(endTime.getHours());
+                        endDate.setMinutes(endTime.getMinutes());
+
+                        scope.listing.StartDate = startDate;
+                        scope.listing.EndDate = endDate;
+
                         scope.picker.opened = false;
                     };
 
@@ -127,6 +155,20 @@
                                 }
                             }
                         }
+                    }
+
+                    scope.updateFrequency = function (day) {
+                        console.log(day);
+                        var frequency = day;
+
+                        for (var i = 0; i < scope.daysOfTheWeek.length; i++) {
+                            if (scope.daysOfTheWeek[i].selected) {
+                                frequency += scope.daysOfTheWeek[i].letter;
+                            }
+                        }
+                        scope.listing.Frequency = frequency;
+
+                        console.log("Update Frequency: " + frequency);
                     }
 
                     function contains(a, obj) {

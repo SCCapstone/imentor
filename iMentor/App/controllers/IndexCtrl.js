27dba0a5a592d49e43bc45292a5c469020e52b64
drthomas <thomas.1728@yahoo.com>
2016-04-08 11:38:06 -0400

    Started work on Issue #35

--- iMentor/App/controllers/IndexCtrl.js
+++ iMentor/App/controllers/IndexCtrl.js
@@ -49,5 +49,55 @@ app.controller('indexCtrl', [ '$scope', '$location', 'manageService',
         {
             $location.path("/EditUser/" + $scope.user.Id);
         }
+
+
+
+        $scope.signedIn = false;
+ 
+    // Here we do the authentication processing and error handling.
+    // Note that authResult is a JSON object.
+    $scope.processAuth = function(authResult) {
+        // Do a check if authentication has been successful.
+        if(authResult['access_token']) {
+            // Successful sign in.
+            $scope.signedIn = true;
+ 
+            console.log("Signed In");
+        } else if(authResult['error']) {
+            // Error while signing in.
+            $scope.signedIn = false;
+ 
+            // Report error.
+        }
+    };
+ 
+    // When callback is received, we need to process authentication.
+    $scope.signInCallback = function(authResult) {
+        $scope.$apply(function() {
+            $scope.processAuth(authResult);
+        });
+    };
+ 
+    // Render the sign in button.
+    $scope.renderSignInButton = function() {
+        gapi.signin.render('signInButton',
+            {
+                'callback': $scope.signInCallback, // Function handling the callback.
+                'clientid': '1086641013362-rj0u1ckimo3hs369gc8q40bvqs2d1rau.apps.googleusercontent.com', // CLIENT_ID from developer console which has been explained earlier.
+                'requestvisibleactions': 'http://schemas.google.com/AddActivity', // Visible actions, scope and cookie policy wont be described now,
+                                                                                  // as their explanation is available in Google+ API Documentation.
+                'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email',
+                'cookiepolicy': 'single_host_origin'
+            }
+        );
+    }
+ 
+    // Start function in this example only renders the sign in button.
+    $scope.start = function() {
+        $scope.renderSignInButton();
+    };
+ 
+    // Call start function on load.
+    $scope.start();
     }
 ]);

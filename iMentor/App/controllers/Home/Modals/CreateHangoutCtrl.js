76443f71b4ff21b86c3eed8580f2126a605d3f25
David Thomas <thomas.1728@yahoo.com>
2016-04-14 15:11:23 -0400

    Fixed bug (issue #88)

--- iMentor/App/controllers/Home/Modals/CreateHangoutCtrl.js
+++ iMentor/App/controllers/Home/Modals/CreateHangoutCtrl.js
@@ -3,16 +3,38 @@ app.controller('createHangoutCtrl', ['$scope', '$mdDialog', 'manageService', 'li
     function createHangoutCtrl($scope, $mdDialog, manageService, listing)
     {
         $scope.listing = listing;
+        var talkgadget = 'https://talkgadget.google.com/hangouts/';
+        var hangout = 'https://hangouts.google.com/call/';
 
-        $scope.cancel = function() {
+        $scope.cancel = function () {
+            $scope.listing.HangoutUrl = null;
             $mdDialog.cancel();
         };
 
         $scope.save = function () {
-            $scope.listing.HangoutStart = new Date();
-            console.log($scope.listing);
-            manageService.updateListing($scope.listing);
-            $mdDialog.hide();
+            if(validate()){
+                $scope.listing.HangoutStart = new Date();
+                manageService.updateListing($scope.listing);
+                $mdDialog.hide();
+            }
         };
+
+        $scope.isValid = function() {
+            validate();
+        }
+
+        function validate() {
+            var toReturn = true;
+            $scope.hangoutForm.hangoutUrl.$setValidity('valid', true);
+
+            if ($scope.hangoutForm.$invalid) {
+                toReturn = false;
+            } else if (!$scope.listing.HangoutUrl.includes(talkgadget) && !$scope.listing.HangoutUrl.includes(hangout)) {
+                toReturn = false;
+                $scope.hangoutForm.hangoutUrl.$setValidity('valid', false);
+            }
+
+            return toReturn;
+        }
     }
 ])

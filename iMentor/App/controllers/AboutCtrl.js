372f85567f5142e5e29a343e1313536af7ca8637
neighbja <neighbja@email.sc.edu>
2016-04-19 14:04:28 -0400

    embedded youtube video

--- iMentor/App/controllers/AboutCtrl.js
+++ iMentor/App/controllers/AboutCtrl.js
@@ -4,5 +4,10 @@ app.controller('aboutCtrl', ['$scope',
     function aboutCtrl($scope) 
     {
 
+        $scope.yt = {
+      width: 600, 
+      height: 480, 
+      videoid: "eCkjTaqIJpc",
+    };
     }
 ])

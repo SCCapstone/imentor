b2720efa9052b94a8aa85a655b9dda856bc5c058
neighbja <neighbja@email.sc.edu>
2016-04-24 20:06:29 -0400

    help page

--- iMentor/App/controllers/AboutCtrl.js
+++ iMentor/App/controllers/AboutCtrl.js
@@ -3,11 +3,7 @@
 app.controller('aboutCtrl', ['$scope', '$location', 'manageService',
     function aboutCtrl($scope, $location, manageService) 
     {
-        $scope.yt = {
-          width: 600, 
-          height: 480, 
-          videoid: "eCkjTaqIJpc",
-        };
+      
 
         getCurrentUser();
 
